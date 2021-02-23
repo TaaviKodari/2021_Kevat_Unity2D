82d6d4021430a406807a721a89142af103ce5288
kodarit11 <taavi.makela@gmail.com>
2021-02-23 19:31:40 +0200

    liikkuminen

--- 2021Kevat_2D_Tasoloikka/Assets/Scripts/PlayerMovement.cs
+++ 2021Kevat_2D_Tasoloikka/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 2; 
-    Rigidbody2D rb2D;
-    
+    public  Rigidbody2D rb2D;
+    private float horizontalMovement;
+
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        //rb2D = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate(){
+        Vector2 playerVelocity = new Vector2(horizontalMovement * moveSpeed, rb2D.velocity.y);
+        rb2D.velocity = playerVelocity;
     }
 }
